ac3e20dd685f0c23437953a8b89871d6df2bbfd4
Haozhe Li <hao-zhe.li@outlook.com>
2025-01-29 12:35:39 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from markdown2 import markdown
+import json
+import os
+
+app = Flask(__name__)
+
+blogs_folder = "blogs"
+
+
+@app.route("/get")
+def home():
+    response = []
+    for blog in os.listdir(blogs_folder):
+        if blog.startswith("."):
+            continue
+        with open(f"{blogs_folder}/{blog}/overview.json", "r") as f:
+            overview = json.load(f)
+        response.append(overview)
+    response.sort(key=lambda x: x["date"], reverse=True)
+    return {"blogs": response}
+
+
+@app.route("/get/<blog_id>")
+def get_blog(blog_id):
+    with open(f"{blogs_folder}/{blog_id}/content.md", "r") as f:
+        blog = markdown(f.read())
+    with open(f"{blogs_folder}/{blog_id}/overview.json", "r") as f:
+        overview = json.load(f)
+    return {"content": blog, "overview": overview}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
